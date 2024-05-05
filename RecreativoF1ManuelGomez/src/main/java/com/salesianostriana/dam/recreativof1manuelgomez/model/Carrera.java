1103e1dc42ac0c854ee2401c4be573e5ad72c24e
Manuel <gomez.maman24@triana.salesianos.edu>
2024-05-05 19:24:10 +0200

    Mas modificaciones en las POJO

--- RecreativoF1ManuelGomez/src/main/java/com/salesianostriana/dam/recreativof1manuelgomez/model/Carrera.java
+++ RecreativoF1ManuelGomez/src/main/java/com/salesianostriana/dam/recreativof1manuelgomez/model/Carrera.java
@@ -1,6 +1,7 @@
 package com.salesianostriana.dam.recreativof1manuelgomez.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -29,4 +30,7 @@ public class Carrera {
 	private boolean asfaltado;
 	private String nombreCircuito;
 	private String localizacion;
+
+	private List<Empleado> listaEmpleados;
+	private List<Componentes> listaComponentes;
 }
--- RecreativoF1ManuelGomez/src/main/java/com/salesianostriana/dam/recreativof1manuelgomez/model/JefeEquipo.java
+++ RecreativoF1ManuelGomez/src/main/java/com/salesianostriana/dam/recreativof1manuelgomez/model/JefeEquipo.java
@@ -1,10 +1,17 @@
 package com.salesianostriana.dam.recreativof1manuelgomez.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Entity
 @SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class JefeEquipo extends Empleado {
 
+	private List<Componentes> listaComponentes;
 }
