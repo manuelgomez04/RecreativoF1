a265df4e21bf57f7fe66bf5456ad23762a27489e
Manuel Gómez <gomez.maman24@triana.salesianos.edu>
2024-05-09 09:06:08 +0200

    Pilotos

--- RecreativoF1ManuelGomez/src/main/java/com/salesianostriana/dam/recreativof1manuelgomez/controllers/PilotoController.java
+++ RecreativoF1ManuelGomez/src/main/java/com/salesianostriana/dam/recreativof1manuelgomez/controllers/PilotoController.java
@@ -1,14 +1,24 @@
 package com.salesianostriana.dam.recreativof1manuelgomez.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.salesianostriana.dam.recreativof1manuelgomez.services.PilotoService;
 
 @Controller
 
 public class PilotoController {
 
+	@Autowired
+	PilotoService pilotoService;
+	
 	@GetMapping("/pilotos")
-	public String showPiloto() {
+	public String showPiloto(Model model) {
+		
+		model.addAttribute("listaPilotos", pilotoService.findAll());
+		
 		return "pilotos";
 	}
 }
