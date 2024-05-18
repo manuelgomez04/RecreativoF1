0783fec41a35353c7378d3da1eed7c10f2f91d9f
Manuel <gomez.maman24@triana.salesianos.edu>
2024-05-18 16:51:08 +0200

    cambios pequeños

--- RecreativoF1ManuelGomez/src/main/java/com/salesianostriana/dam/recreativof1manuelgomez/controllers/InicioSesionController.java
+++ RecreativoF1ManuelGomez/src/main/java/com/salesianostriana/dam/recreativof1manuelgomez/controllers/InicioSesionController.java
@@ -1,20 +1,17 @@
 package com.salesianostriana.dam.recreativof1manuelgomez.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import com.salesianostriana.dam.recreativof1manuelgomez.services.PresupuestoService;
 
 @Controller
 public class InicioSesionController {
 
-	@Autowired
-	private PresupuestoService presupuestoService;
+//	@Autowired
+//	private PresupuestoService presupuestoService;
 
 	@GetMapping("/")
 	public String showInicio() {
-		presupuestoService.modifyPresupInicial();
+		// presupuestoService.modifyPresupInicial();
 
 		return "inicioSesion";
 	}
