87d9cc143a4ff6b8158ce255ca1cfde0247bfb8e
Manuel <gomez.maman24@triana.salesianos.edu>
2024-05-19 03:17:20 +0200

    Rama acabada

--- RecreativoF1ManuelGomez/src/main/resources/static/JS/modalBorrar.js
+++ RecreativoF1ManuelGomez/src/main/resources/static/JS/modalBorrar.js
@@ -1,13 +1,12 @@
-function abrirModal(modalId, pilotoId) {
-    var modal = document.getElementById(modalId);
-    modal.addEventListener('show.bs.modal', function (event) {
-        var button = event.relatedTarget;
-        var nombre = button.parentNode.previousElementSibling.firstChild.innerText;
-        var modalBody = modal.querySelector('.modal-body');
-        var deleteBtn = modal.querySelector('#deleteModalConfirmBtn');
-        deleteBtn.setAttribute('href', '/pilotos/borrar/' + pilotoId);
-        modalBody.innerHTML = '¿Estás seguro de que deseas borrar a ' + nombre + '?';
+document.addEventListener('DOMContentLoaded', function () {
+    let deleteLinks = document.querySelectorAll('.delete-link');
+    deleteLinks.forEach(function (deleteLink) {
+        deleteLink.addEventListener('click', function (event) {
+            event.preventDefault();
+            let modal = new bootstrap.Modal(document.getElementById('confirmDeleteModal'), {});
+            modal.show();
+            let deleteUrl = deleteLink.getAttribute('href');
+            document.getElementById('deleteLink').setAttribute('href', deleteUrl);
+        });
     });
-    var modalInstance = new bootstrap.Modal(modal, { keyboard: false });
-    modalInstance.show();
-}
+});
